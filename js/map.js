b0f6245f02a803e300e74e9c815991a06f7e80f2
Melamara <v.khanagua@ya.ru>
2017-04-23 18:23:54 +0300

    module5-task2

--- js/map.js
+++ js/map.js
@@ -71,5 +71,58 @@
     }
   }
 
+  // Перетаскивание пина по карте пользователем
+  window.pinHandle = window.map.querySelector('.pin__main');
+
+  window.pinHandle.addEventListener('mousedown', function (evt) {
+    // получаем первычные координаты пина
+    evt.preventDefault();
+    var pinCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    // получаем новые координаты
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: pinCoords.x - moveEvt.clientX,
+        y: pinCoords.y - moveEvt.clientY
+      };
+
+      pinCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      // перемещаем пин по новым координатам
+      window.pinHandle.style.left = (window.pinHandle.offsetLeft - shift.x) + 'px';
+      window.pinHandle.style.top = (window.pinHandle.offsetTop - shift.y) + 'px';
+
+      // передаем координаты в форму с поправкой на хвостик пина
+      window.pinHandleWidth = window.pinHandle.offsetWidth;
+      window.pinHandleHeight = window.pinHandle.offsetHeight;
+      var actualX = window.pinHandle.offsetLeft - shift.x + (window.pinHandleWidth / 2);
+      var actualY = window.pinHandle.offsetTop - shift.y + window.pinHandleHeight;
+
+      if ((actualX <= 1210) && (actualY <= 645)) {
+        window.address.value = 'x: ' + actualX + ', y: ' + actualY;
+      } else {
+        alert('Установите метку в границах карты'); //  почему здесь ругается еслинт на алерт?
+        window.map.removeEventListener('mousemove', onMouseMove);
+        window.map.removeEventListener('mouseup', onMouseUp);
+      }
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      window.map.removeEventListener('mousemove', onMouseMove);
+      window.map.removeEventListener('mouseup', onMouseUp);
+    };
+
+    window.map.addEventListener('mousemove', onMouseMove);
+    window.map.addEventListener('mouseup', onMouseUp);
+  });
 
 })();
