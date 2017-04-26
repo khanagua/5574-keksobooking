22c30b6e042b99c484e100c5e01bc571ea7c8aeb
Виктория Ханагуа <v.khanagua@ya.ru>
2017-04-26 11:47:56 +0300

    module6-task1

---
+++ js/load.js
@@ -0,0 +1,59 @@
+'use strict';
+
+(function () {
+  /**
+  * Сделать запрос на сервер
+  * @param {string} url адрес хоста
+  * @param {function} onLoad функция обработки при положительном ответе
+  */
+  window.load = function (url, onLoad) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      switch (xhr.status) {
+        case 200:
+          onLoad(xhr.response);
+          break;
+        case 400:
+          onError('Неверный запрос');
+          break;
+        case 401:
+          onError('Пользователь не авторизован');
+          break;
+        case 404:
+          onError('Ничего не найдено');
+          break;
+        default:
+          onError('Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Время ожидания превысило ' + xhr.timeout + ' мс');
+    });
+
+    xhr.timeout = 10000; // 10s
+
+    xhr.open('GET', URL);
+    xhr.send();
+  };
+
+  /**
+  * Обработать отрицательный ответ с сервера
+  * @param {string} errorMessage текст сообщения об ошибке
+  */
+  function onError(errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; border: 2px inset black; background: #f0f0ea; width: 1196px;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '20px';
+    node.textContent = errorMessage;
+    var beforeBeginNode = document.querySelector('.tokyo__filters-container');
+    beforeBeginNode.insertAdjacentElement('beforebegin', node);
+  }
+})();
